74c0f778d0b05c77cbeb4d8744b034af6bcfc93c
Damian Olivo <146010485+DamianRoc13@users.noreply.github.com>
2026-01-22 09:12:59 -0500

    fumadocs-seo

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -13,8 +13,8 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
 
   const MDX = page.data.body;
   const gitConfig = {
-    user: 'username',
-    repo: 'repo',
+    user: 'DamianRoc13',
+    repo: 'docs-gitaf',
     branch: 'main',
   };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,24 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
+import { RootProvider } from 'fumadocs-ui/provider/next'; 
 import './global.css';
 import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export const metadata: Metadata = {
+  title: {
+    default: 'Gitaf Docs',
+    template: '%s | Gitaf Docs',
+  },
+  description: 'Documentación oficial de GITAF: Sistema de Orientación Audible Inalámbrico para el Entrenamiento de Fútbol Adaptado.',
+  metadataBase: new URL('https://docs.gitafpro.com'),
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="es" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
